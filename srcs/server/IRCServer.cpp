d5b928772cad1d46826f4d774cce75246b4bd90e
donghyuk <90084199+reg0145@users.noreply.github.com>
2023-05-26 17:21:59 +0900

    [Feax] kqueue 기능(EVFILT_WRITE)추가 close #70 (#71)
    * [Fix] kqueue 기능(EVFILT_WRITE)추가 close #70
    
    * [Fix] EVFILT_WRITE 또는 EV_ERROR 처리 수정 #70
    
    * [Fix] 공백수정 #70
    
    * [Fix] process함수 다시 복구 #70
    
    * [Fix] EVFILT_WRITE 또는 EV_ERROR 처리 수정 추가 #70

--- srcs/server/IRCServer.cpp
+++ srcs/server/IRCServer.cpp
@@ -52,7 +52,12 @@ void IRCServer::onRequestHandler(int socket)
 	}
 	else
 	{
-		_sessionManager.getSessionBySocket(socket)->onReadable();
+		Session* session = _sessionManager.getSessionBySocket(socket);
+
+		if (session != NULL)
+		{
+			session->onReadable();
+		}
 	}
 }
 
@@ -65,7 +70,20 @@ void IRCServer::onRequestErrorHandler(int socket)
 	}
 	else
 	{
-		_sessionManager.unRegisterSessionBySocket(socket);
+		Session* session = _sessionManager.getSessionBySocket(socket);
+
+		if (session)
+		{
+			IRCMessage message;
+			message._command = "QUIT";
+
+			_packetManager.process(session->getSessionIndex(), message);
+			_sessionManager.unRegisterSessionBySocket(socket);
+		}
+		else
+		{
+			close(socket);
+		}
 	}
 }
 
